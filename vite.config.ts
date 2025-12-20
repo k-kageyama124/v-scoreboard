5e6cb1f761a275bd0a11bf9f287423ab36a29523
k-kageyama124 <k.kageyama@finfinity.co.jp>
2025-12-20 22:49:36 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/v-scoreboard/',  // ← この行を追加
+  base: '/v-scoreboard/',  // ← この行を追加！
   server: {
     host: true
   }
