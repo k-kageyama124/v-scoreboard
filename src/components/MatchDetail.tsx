8daeed2f4315b61fb3065bcb67770e4d4128532b
k-kageyama124 <k.kageyama@finfinity.co.jp>
2026-02-15 19:17:06 +0900

    Update MatchDetail.tsx

--- src/components/MatchDetail.tsx
+++ src/components/MatchDetail.tsx
@@ -262,7 +262,7 @@ undefined
     link.click();
   };
 
-    if (!currentSetData) {
+     if (!currentSetData) {
     return (
       <div className="min-h-screen bg-gradient-to-br from-purple-50 to-blue-50 p-4">
         <div className="max-w-6xl mx-auto">
